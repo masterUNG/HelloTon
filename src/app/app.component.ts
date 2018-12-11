8e4a3096d367ed3d9b7a29c83f8e7a8846ae22f7
masterUNG <phrombutr@gmail.com>
2018-12-11 14:35:47 +0700

    Deploy Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent implements OnInit{
   title = 'I Love Thailand 1234';
 
   // Explicit
-  private myTitle: string;  // Demo Explicit Variable Type String
-  private myColor: string;
-  private myNumber;
-  private mySize: string;
+   myTitle: string;  // Demo Explicit Variable Type String
+   myColor: string;
+   myNumber;
+   mySize: string;
 
   constructor() {}
 
   ngOnInit(): void {
     
     this.myTitle = "Doramon";
-    this.myColor = '#FF0000';
+    this.myColor = '#FFFF00';
     this.myNumber = 0;
-    this.mySize = '60px'
+    this.mySize = '100px'
 
   }
 
